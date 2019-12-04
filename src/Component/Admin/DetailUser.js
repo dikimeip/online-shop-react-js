d2cd02f8dbdc563826b892c0d076d40b75c60185
dikimeip <pm159145@gmail.com>
2019-12-04 08:10:27 +0700

    menambahkan file detail user di halaman admin

---
+++ src/Component/Admin/DetailUser.js
@@ -0,0 +1,21 @@
+import React,{Component} from 'react'
+import NavbarA from './NavbarA'
+
+class DetailUser extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <NavbarA/>
+            </div>
+        )
+    }
+}
+
+export default DetailUser
